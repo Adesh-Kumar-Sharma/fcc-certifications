633c100ee1df393340a5bf01c01d30e949d11ea6
Adesh Kumar Sharma <74715554+Adesh-Kumar-Sharma@users.noreply.github.com>
2025-11-10 04:28:30 +0530

    Add files via upload

---
+++ Relational Database/Project 4/element.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+INPUT="$1"
+
+RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+FROM elements e
+JOIN properties p USING (atomic_number)
+JOIN types t USING (type_id)
+WHERE e.atomic_number::text = '$INPUT' OR lower(e.symbol) = lower('$INPUT') OR lower(e.name) = lower('$INPUT') LIMIT 1;")
+
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
